d63c82a5bcf166ed68e312874f3d6fece3ea6e0e
027enes <enesinan.27@hotmail.com>
2024-05-04 14:46:14 +0300

    gurme

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="category in categories.data" :key="category.id">
+            <h2>{{ category.title }}</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    async function CategoriesList(){
+        const { data : categories } = await useFetch('https://dev.gurmenu.com/api/v2/restaurant/categories', {
+        method: 'POST',
+
+    })
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
