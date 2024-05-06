7364d77ce59f7c65052f928221330928a03d36b7
Mustafa Afat <afatmustafa@gmail.com>
2024-05-06 17:07:46 +0300

    Update [slug].vue

--- pages/categories/[slug].vue
+++ pages/categories/[slug].vue
@@ -2,20 +2,25 @@
     <div>
       <section id="products-content">
       <div class="container">
-        <div class="p-products-items products-page-items" >
-          <div class="p-products-item " id="openSheetgunun-corbasi11474" aria-controls="sheet" v-for="p in category.data.items" :key="p.id">
-            <div class="prod-cont">
-              <div class="p-products-top">
-                <div class="products-item-img">
-                  <img :src="p.medias.cover?.url" alt="product image" />
-                </div>
-                <div class="products-item-text">
-                  <h2 class="products-titlee">{{ p.title }}</h2>
-                  <h2 class="products-sell">{{p.price}} {{ p.currency }}</h2>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div class="p-products-items products-page-items">
+          <template v-if="categoryPending">
+            Kategoriler bekleniyor...
+          </template>
+         <template v-else>
+           <div class="p-products-item " id="openSheetgunun-corbasi11474" aria-controls="sheet" v-for="p in category.data.items" :key="p.id">
+             <div class="prod-cont">
+               <div class="p-products-top">
+                 <div class="products-item-img">
+                   <img :src="p.medias.cover?.url" alt="product image" />
+                 </div>
+                 <div class="products-item-text">
+                   <h2 class="products-titlee">{{ p.title }}</h2>
+                   <h2 class="products-sell">{{p.price}} {{ p.currency }}</h2>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </template>
         </div>
       </div>
     </section>
@@ -28,7 +33,7 @@
   const route = useRoute();
   const slug = route.params.slug || '';
   const uri = `https://dev.gurmenu.com/api/v2/restaurant/category/`;
-  const { data: category } = await useFetch(uri, {
+  const { data: category, pending: categoryPending } = await useFetch(uri, {
     key: slug,
     method: 'POST',
     body: {
