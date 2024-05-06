3fab44c5e6a44917f8e98f5096a36d28b3a02d48
027enes <enesinan.27@hotmail.com>
2024-05-06 09:53:52 +0300

    Update index.vue

--- components/Category/index.vue
+++ components/Category/index.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-
-    <NuxtLink :to="'/products/' + category.slug" :key="category.slug" class="products-item  sag-bg" >
-      <div class="products-img">
-        <img alt="ATIŞTIRMALIKLAR" :src="category.medias.cover.url" loading="lazy">
+    <div>
+      <div>
+  
+      <NuxtLink :to="'/products/' + category.slug" :key="category.slug" class="products-item  sag-bg" >
+        <div class="products-img">
+          <img alt="ATIŞTIRMALIKLAR" :src="category.medias.cover.url" loading="lazy"/>
+        </div>
+        <div class="product-text sag">
+          <h3>{{ category.title }}</h3>
+        </div>
+      </NuxtLink>
+  
+    </div>
       </div>
-      <div class="product-text sag">
-        <h3>{{ category.title }}</h3>
-      </div>
-    </NuxtLink>
-
-  </div>
-</template>
-
-<script setup>
-const {category} = defineProps(['category']);
-</script>
-
-<style lang="scss" scoped>
-
+  </template>
+  
+  <script setup>
+  const {category} = defineProps(['category']);
+  
+  </script>
+  
+  <style lang="scss" scoped>
 </style>
+  
+  
