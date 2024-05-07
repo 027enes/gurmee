85bcf1a2d7f295f245a8bca9ccef56ab8e2130e1
027enes <enesinan.27@hotmail.com>
2024-05-07 14:54:45 +0300

    wip

--- components/About/AboutSocials/index.vue
+++ components/About/AboutSocials/index.vue
@@ -7,8 +7,9 @@
             class="aboutSlider"
             :loop="false"
             :modules="[ Autoplay, Scrollbar]"
-            :slides-per-view="5"
+            :slides-per-view="slidesPerView"
             :space-between="20"
+            :breakpoints="breakpoints"
             :scrollbar="{ draggable: true, hide: false }" 
             @slideChange="onSlideChange"
             @swiper="onSwiper"
@@ -35,6 +36,28 @@ import {Pagination, Autoplay, Scrollbar } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/scrollbar';  
+
+
+import { ref } from 'vue';
+const slidesPerView = ref(2);
+const breakpoints = ref({
+  // when window width is <= 768px
+  768: {
+    slidesPerView: 2,
+  },
+  // when window width is <= 1024px
+  1024: {
+    slidesPerView: 3,
+  },
+  // when window width is <= 1200px
+  1200: {
+    slidesPerView: 4,
+  },
+  // when window width is <= 1600px
+  1600: {
+    slidesPerView: 5,
+  },
+});
 
 </script>
 
