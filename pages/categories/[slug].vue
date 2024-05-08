05283ab9b04c67987dae008d20b1d6e36c5d63a6
027enes <enesinan.27@hotmail.com>
2024-05-08 11:40:37 +0300

    error

--- pages/categories/[slug].vue
+++ pages/categories/[slug].vue
@@ -2,9 +2,15 @@
     <div v-if="pending">
         <h1>Loading...</h1>
     </div>
-      <div v-else-if="error">
-        <h1>There was an error</h1>
-        <pre>{{ error }}</pre>
+      <div v-else-if="error" class="error">
+          <nuxt-icon name="error" filled class="icon-font error-icon"></nuxt-icon>
+          <h1 class="error-title">
+            Aradığınız sayfa bulunamadı.
+          </h1>
+          <h2 class="error-subtitle">
+            Kapatıp tekrar açmayı denediniz mi?
+          </h2>
+          <NuxtLink to="/" class="errorBtn">Anasayfaya Dön</NuxtLink>
       </div>
     <div v-else>
       <Banner v-if="!categoryPending" :title="category.data.title" :image="category.data.medias.cover.url"/>
